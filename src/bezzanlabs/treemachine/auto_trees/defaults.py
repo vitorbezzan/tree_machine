5f25bc580bb9ddbfe0d7c915d0e9f620a419e5ba
Vitor Bezzan <vitor@bezzan.com>
2024-07-24 13:14:16 -0300

    Adding more type checks

--- src/bezzanlabs/treemachine/auto_trees/defaults.py
+++ src/bezzanlabs/treemachine/auto_trees/defaults.py
@@ -2,7 +2,7 @@
 """
 Minimal configuration file for Auto trees.
 """
-from typing import TypeAlias
+import typing as tp
 from optuna.distributions import (
     CategoricalDistribution,
     FloatDistribution,
@@ -10,33 +10,55 @@ from optuna.distributions import (
 )
 from pydantic import validate_call
 
-
-TUsrDistribution: TypeAlias = dict[str, tuple[int, int] | tuple[float, float] | list]
-TDistribution: TypeAlias = dict[
+TDistribution: tp.TypeAlias = dict[
     str, CategoricalDistribution | IntDistribution | FloatDistribution
 ]
 
+_FloatLike = tp.Union[tuple[float, float], tp.Sequence[float]]
+_IntLike = tp.Union[tuple[int, int], tp.Sequence[int]]
+
+
+class TUsrDistribution(tp.TypedDict, total=False):
+    """
+    Defines acceptable hyperparameters and their respective types for bounds when
+        searching for the best model.
+
+    Please see https://xgboost.readthedocs.io/en/latest/parameter.html for more details
+        on these parameters work in your model.
+    """
+    eta: _FloatLike
+    gamma: _FloatLike
+    reg_alpha: _FloatLike
+    colsample_bytree: _FloatLike
+    colsample_bylevel: _FloatLike
+    colsample_bynode: _FloatLike
+    reg_lambda: _FloatLike
+    max_depth: _IntLike
+    n_estimators: _IntLike
+
+
 defaults: TUsrDistribution = {
-    "alpha": (0.0, 1000.0),
+    "reg_alpha": (0.0, 1000.0),
     "colsample_bytree": (0.5, 1.0),
-    "lambda": (0.0, 1000.0),
+    "reg_lambda": (0.0, 1000.0),
     "max_depth": (2, 6),
     "n_estimators": (1, 1000),
 }
 
 
 @validate_call(config={"arbitrary_types_allowed": True})
-def get_param_distributions(params: TUsrDistribution) -> TDistribution:
+def get_param_distributions(user_params: TUsrDistribution) -> TDistribution:
     """
     Returns distribution dictionary for parameters given user input.
     """
     params_: TDistribution = {}
-    for param_name, domain in params.items():
-        if isinstance(domain, list):
+    for param_name, domain in user_params.items():
+        if isinstance(domain, tp.Sequence):
             params_[param_name] = CategoricalDistribution(domain)
-        elif isinstance(domain[0], int):
-            params_[param_name] = IntDistribution(domain[0], int(domain[1]))
-        else:
-            params_[param_name] = FloatDistribution(domain[0], float(domain[1]))
+        elif isinstance(domain, tuple):
+            if isinstance(domain[0], int):
+                params_[param_name] = IntDistribution(domain[0], int(domain[1]))
+            else:
+                params_[param_name] = FloatDistribution(domain[0], float(domain[1]))
 
     return params_
